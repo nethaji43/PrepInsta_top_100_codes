2b4f9a8869b949c88fba8dd6531b034dba80c4c8
nethaji <mudhigolamnethaji19@gmail.com>
2025-06-20 07:09:38 +0530

    leap year code

---
+++ 1.Basic_coding/Leap_year_or_not.py
@@ -0,0 +1,5 @@
+year=int(input("enter any year as your wish?"))
+if (year%400==0) or (year%100!=0 and year%4==0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
